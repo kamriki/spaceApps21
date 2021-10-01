75fc50e64b630270aee95aa6cbe19880fa166152
Khaled Amriki <kamriki@gmail.com>
2021-10-01 12:00:25 +0300

    background intro video

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,16 +1,17 @@
 import { SharedModule } from '../../common/shared.module';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { HomePage } from './home.page';
-
 import { HomePageRoutingModule } from './home-routing.module';
+import 'gl-ionic-background-video';
 
 
 @NgModule({
   imports: [
     CommonModule, IonicModule, HomePageRoutingModule, SharedModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomePageModule {}
