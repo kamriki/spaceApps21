846a965bae6b8568c3fd22b84a588cc03ac7918b
Khaled Amriki <kamriki@gmail.com>
2021-10-02 10:50:04 +0300

    sun screen

--- src/app/pages/sun/sun.page.ts
+++ src/app/pages/sun/sun.page.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-const */
 import { Component, OnInit } from '@angular/core';
 import { Copyright } from 'src/app/common/types';
 
@@ -8,12 +9,24 @@ import { Copyright } from 'src/app/common/types';
 })
 export class SunPage implements OnInit {
   public copyright: Copyright = {
-    name: 'esa', url: 'https://www.esa.int/ESA_Multimedia/Images/2020/01/Missions_studying_the_Sun'
+    name: 'NASA', url: 'https://solarsystem.nasa.gov/solar-system/sun/overview/'
   };
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  openWebpage(page: string) {
+    let url = '';
+    switch(page) {
+      case 'black-spots': url = 'https://solarsystem.nasa.gov/solar-system/sun/overview/'; break;
+      case 'parker': url = 'https://www.esa.int/Science_Exploration/Space_Science/Solar_Orbiter/Teamwork_with_Parker_Solar_Probe'; break;
+      case 'solar-winds': url = 'https://www.jpl.nasa.gov/nmp/st5/SCIENCE/solarwind.html'; break;
+      case 'solar-orbiter': url = 'https://www.esa.int/Science_Exploration/Space_Science/Solar_Orbiter'; break;
+      case 'sun': url = 'https://solarsystem.nasa.gov/solar-system/sun/overview/'; break;
+    }
+    window.open(url, '_system', 'location=yes');
   }
 
 }
